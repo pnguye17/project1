8ab4b9873a1e588dbb275abce676b1ce705c5822
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-03 23:20:10 -0700

    add mole holes

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log("it works")
+
+const $holes = document.querySelector(".holes");
+console.log($holes)
+
+// const $container = document.querySelector(".container");
+// console.log($container)
+// for (let i = 0; i < 8; i++){
+//     $holes.append($holes)
+// }
+
