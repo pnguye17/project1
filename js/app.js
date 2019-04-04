c9a5d2452e7b9a106434435b6324fc99e24a0182
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-04 16:27:56 -0700

    add motion to gophers/moles

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
-console.log("it works")
+/*----- constants -----*/ 
+// player, mole/ player class, score, timer
 
-const $holes = document.querySelector(".holes");
-console.log($holes)
+class Moles {
+    constructor(health){
+        this.health = Math.floor(Math.random() * 9) + 1
+    }
+    hide() {
+        console.log("the mole is hidden")
+    }
+    // bite() {
+    //     console.log("the hammer missed and the mole just bite you")
+    // }
+}
 
-// const $container = document.querySelector(".container");
-// console.log($container)
-// for (let i = 0; i < 8; i++){
-//     $holes.append($holes)
-// }
+class Player {
+    constructor(name) {
+        this.name = name
+        this.score = 0
+        this.hitMiss = 0
+    }
+    bonk() {
+        console.log(`${this.name} just swung his hammer`)
+        // maybe add an animation that shakes the screen?
+    }
+}
 
+const player = new Player("peter")
+player.bonk();
+
+
+let moleArmy = [];
+
+
+/*----- app's state (variables) -----*/ 
+// mole holes in align in matrix 
+/*----- cached element references -----*/
+
+// score
+/*----- event listeners -----*/ 
+// when the moles are hit
+/*----- functions -----*/
+// attack/hit, hid (for the moles)
+// startGame, countdowm timer, randomhole
