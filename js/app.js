0b575a9ef93ff7c648058596d29c5594d54eb458
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-09 03:04:18 -0700

    add functionality

--- js/app.js
+++ js/app.js
@@ -1,80 +1,79 @@
-/*----- constants -----*/ 
-// player, mole/ player class, score, timer
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const btnStart = document.querySelector('button');
+const bonkSound = document.querySelector('audio');
+const startScreen = document.querySelector('.start-screen');
+const showScore = document.querySelector('.show-score');
 
-class Moles {
-    constructor(health){
-        this.health = Math.floor(Math.random() * 9) + 1
-        this.speed = Math.floor(Math.random() * 9) + 1
-    }
-    hide() {
-        console.log("the mole is hidden")
-    }
-    // bite() {
-    //     console.log("the hammer missed and the mole just bite you")
-    // }
+let score = 0;
+let lastHole;
+let timeUp = false;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
 
-class Player {
-    constructor(name) {
-        this.name = name
-        this.score = 0
-        this.hitMiss = 0
-        this.health = 10
-    }
-    bonk() {
-        console.log(`${this.name} just swung his hammer`)
-        Moles.health -= 1
-        this.score += 1
-        // maybe add an animation that shakes the screen?
-    }
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
 }
 
-const player = new Player("peter")
-player.bonk();
+function peep() {
+  const time = randomTime(300, 1000);
+  const hole = randomHole(holes);
 
+  hole.classList.add('up');
 
-let moleArmy = [];
-
-const moleMaking = (num) => {
-    for (let i = 0; i < num; i++) {
-        const moleBabbies = new Moles
-        moleArmy.push(moleBabbies)
-    }
+  setTimeout(() => {
+    hole.classList.remove('up');
+    scoreBoard.classList.remove('add');
+    if (!timeUp) peep();
+  }, time);
 }
 
 
+function start() {
+  score = 0;
+  scoreBoard.textContent = score;
+  timeUp = false;
+  scoreBoard.classList.remove('add');
+  startScreen.classList.add('hide');
 
-const creatingHoles = (num) => {
-    for (let k = 0; k < num; k++) {
-        const $molehole = $('<img id="mole">')
-        $molehole.attr("src", "photos_project1/maxresdefault.jpg")
-        $molehole.attr("width", "200")
-        $(".ground").append($molehole)
+  // start peep
+  peep();
+
+  setTimeout(() => {
+    timeUp = true;
+    startScreen.classList.remove('hide');
+
+    if (score > 0) {
+      showScore.classList.add('show');
+      const message = 'Your score: ' + score + (score >= 10 ? " GREAT!" : '');
+      showScore.textContent = message;
     }
+
+  }, 10000);
 }
 
-moleMaking(3)
-creatingHoles(3)
+function bonk(e) {
+  bonkSound.currentTime = 0;
+  if (!timeUp) {
+    bonkSound.play();
+    scoreBoard.classList.add('add');
+    score++;
+    scoreBoard.textContent = score;
+  }
+}
 
+// moles.forEach(mole => {
+//   mole.addEventListener('click', bonk);
+// });
 
-
-
-
-
-/*----- app's state (variables) -----*/ 
-// mole holes in align in matrix 
-/*----- cached element references -----*/
-
-// score
-/*----- event listeners -----*/ 
-const $moles = $(".container");
-
-$moles.on("click", () => {
-    player.bonk
-})
-
-console.log(player.score)
-// when the moles are hit
-/*----- functions -----*/
-// attack/hit, hid (for the moles)
-// startGame, countdowm timer, randomhole
+// btnStart.addEventListener('click', start);
