85b341b9ba50cc433f2a8ea96cb28947c42ec0f4
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-09 15:06:54 -0700

    must animate the moles

--- js/app.js
+++ js/app.js
@@ -1,79 +1,54 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-const btnStart = document.querySelector('button');
-const bonkSound = document.querySelector('audio');
-const startScreen = document.querySelector('.start-screen');
-const showScore = document.querySelector('.show-score');
+const $holes = $('.hole');
+const $scoreBoard = $('#score');
+const $moles = $('.mole');
+const $bonkSound = $('audio');
+
 
 let score = 0;
 let lastHole;
-let timeUp = false;
+
+$(".game").on("click", (event) => {
+    console.log(event.target)
+    if ($(event.target).hasClass("mole")) {
+        score += 1
+        $scoreBoard.text(score)
+    }
+   
+})
 
 function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function randomHole(holes) {
-  const idx = Math.floor(Math.random() * holes.length);
-  const hole = holes[idx];
+function randomHole($holes) {
+  const index = Math.floor(Math.random() * $holes.length);
+  const hole = $holes[index];
 
   if (hole === lastHole) {
-    return randomHole(holes);
+    return randomHole($holes);
   }
   lastHole = hole;
   return hole;
 }
 
-function peep() {
+function pop() {
   const time = randomTime(300, 1000);
-  const hole = randomHole(holes);
+  const hole = randomHole($holes);
+    console.log(time,hole)
+   hole.addClass("up")
 
-  hole.classList.add('up');
-
-  setTimeout(() => {
-    hole.classList.remove('up');
-    scoreBoard.classList.remove('add');
-    if (!timeUp) peep();
-  }, time);
+//   setTimeout(() => {
+//     hole.removeClass('up');
+//     $scoreBoard.removeClass('add');
+//     if (!timeUp) peep();
+//   }, time);
 }
 
 
-function start() {
-  score = 0;
-  scoreBoard.textContent = score;
-  timeUp = false;
-  scoreBoard.classList.remove('add');
-  startScreen.classList.add('hide');
-
-  // start peep
-  peep();
-
-  setTimeout(() => {
-    timeUp = true;
-    startScreen.classList.remove('hide');
-
-    if (score > 0) {
-      showScore.classList.add('show');
-      const message = 'Your score: ' + score + (score >= 10 ? " GREAT!" : '');
-      showScore.textContent = message;
-    }
-
-  }, 10000);
-}
-
-function bonk(e) {
-  bonkSound.currentTime = 0;
-  if (!timeUp) {
-    bonkSound.play();
-    scoreBoard.classList.add('add');
-    score++;
-    scoreBoard.textContent = score;
-  }
-}
-
-// moles.forEach(mole => {
-//   mole.addEventListener('click', bonk);
-// });
-
-// btnStart.addEventListener('click', start);
+// function start() {
+//   score = 0;
+//   scoreBoard.textContent = score;
+//   timeUp = false;
+//   scoreBoard.removeClass('add');
+//   startScreen.addClass('hide');
+// }
