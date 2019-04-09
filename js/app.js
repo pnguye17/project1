6e5df170ea6e2454cb20fe2abd7faa542db8089c
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-08 21:14:04 -0700

    add functions and resdeign layout

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 class Moles {
     constructor(health){
         this.health = Math.floor(Math.random() * 9) + 1
+        this.speed = Math.floor(Math.random() * 9) + 1
     }
     hide() {
         console.log("the mole is hidden")
@@ -18,9 +19,12 @@ class Player {
         this.name = name
         this.score = 0
         this.hitMiss = 0
+        this.health = 10
     }
     bonk() {
         console.log(`${this.name} just swung his hammer`)
+        Moles.health -= 1
+        this.score += 1
         // maybe add an animation that shakes the screen?
     }
 }
@@ -31,6 +35,31 @@ player.bonk();
 
 let moleArmy = [];
 
+const moleMaking = (num) => {
+    for (let i = 0; i < num; i++) {
+        const moleBabbies = new Moles
+        moleArmy.push(moleBabbies)
+    }
+}
+
+
+
+const creatingHoles = (num) => {
+    for (let k = 0; k < num; k++) {
+        const $molehole = $('<img id="mole">')
+        $molehole.attr("src", "photos_project1/maxresdefault.jpg")
+        $molehole.attr("width", "200")
+        $(".ground").append($molehole)
+    }
+}
+
+moleMaking(3)
+creatingHoles(3)
+
+
+
+
+
 
 /*----- app's state (variables) -----*/ 
 // mole holes in align in matrix 
@@ -38,6 +67,13 @@ let moleArmy = [];
 
 // score
 /*----- event listeners -----*/ 
+const $moles = $(".container");
+
+$moles.on("click", () => {
+    player.bonk
+})
+
+console.log(player.score)
 // when the moles are hit
 /*----- functions -----*/
 // attack/hit, hid (for the moles)
