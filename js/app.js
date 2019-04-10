060131a89f71eceecd59424192813fa2f787d673
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-10 09:03:16 -0700

    delete the sugar

--- js/app.js
+++ js/app.js
@@ -1,55 +1,56 @@
 // global variables
 
-const holes = document.querySelectorAll(".hole");
-const scoreBoard = document.querySelector(".score");
-const moles = document.querySelectorAll(".mole");
+const $holes = document.querySelectorAll(".hole");
+const $scoreboard = document.querySelector(".score");
+const $moles = document.querySelectorAll(".mole");
 
 
 let previousHole;
-let timeUp = false;
+let timeUp = false; //<===== initiating the as false bc game hasnt started
 let score = 0;
 
 // eventlistener
-moles.forEach(mole => mole.addEventListener("click", hit)); // <=== adding listeners to each mole div
+$moles.forEach(mole => mole.addEventListener("click", hit)); // <=== adding listeners to each mole div
 
 // functions
 
-
+// get a random time the $moles would pop milisecs
 function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
   
 }
 
-function randomHole(holes) {
-  const i = Math.floor(Math.random() * holes.length);
-  const hole = holes[i];
-  if (hole === previousHole) { //<= if the holes are the same run the function again
-    return randomHole(holes);
+
+function randomHole($holes) {
+  const i = Math.floor(Math.random() * $holes.length);
+  const hole = $holes[i];
+  if (hole === previousHole) { //<= if the $holes are the same run the function again
+    return randomHole($holes);
   }
   lastHole = hole;
   return hole;
 }
 
-// functions to make moles appear
+// functions to make $moles appear
 function pop() {
-  const time = randomTime(200, 2000);
-  const hole = randomHole(holes);
-  hole.classList.add("up");
+  const time = randomTime(200, 1000);
+  const hole = randomHole($holes);
+  hole.classList.add("up"); //<=== to animate the $moles
   setTimeout(() => {
     hole.classList.remove("up");
-    if (!timeUp) pop();
-  }, time);
+    if (!timeUp) pop(); //<=== ends the loop
+  }, time); //<=== 
 }
 
 function hit(e) {
   score++;
   this.parentNode.classList.remove("up");
-  scoreBoard.textContent = score;
+  $scoreboard.textContent = score;
 }
 
 
 function startGame() {
-  scoreBoard.textContent = 0;
+  $scoreboard.textContent = 0;
   timeUp = false;
   score = 0;
   pop();
